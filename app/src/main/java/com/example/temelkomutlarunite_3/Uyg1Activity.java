0e709f7ed0e119d17ec8e7d48220e49fa1dc496b
Serkan Aydın <aydinserkan2004@gmail.com>
2022-10-28 23:20:05 +0300

    28.10.2022 ilk iki örnek yapıldı main fonksiyonu parametresiz bir şekilde çağrıldığında çalışıyor

---
+++ app/src/main/java/com/example/temelkomutlarunite_3/Uyg1Activity.java
@@ -0,0 +1,23 @@
+package com.example.temelkomutlarunite_3;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Uyg1Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.uyg1_activity);
+
+        main();
+    }
+
+    public static void main() {
+        boolean degisken1 = true;
+        boolean degisken2 = false;
+        System.out.println(degisken1);
+        System.out.println(degisken2);
+    }
+}
---
+++ app/src/main/java/com/example/temelkomutlarunite_3/Uyg2Activity.java
@@ -0,0 +1,28 @@
+package com.example.temelkomutlarunite_3;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Uyg2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.uyg2_activity);
+
+        main();
+    }
+
+    public static void main() {
+        byte kucukSayi = 127;
+        short kisaSayi = 32727;
+        int tamSayi = 2147483647;
+        long uzunSayi = 9223372036854775807L;
+        System.out.println("Byte: " + kucukSayi);
+        System.out.println("Short: " + kisaSayi);
+        System.out.println("Int: " + tamSayi);
+        System.out.println("Long: " + uzunSayi);
+    }
+}
